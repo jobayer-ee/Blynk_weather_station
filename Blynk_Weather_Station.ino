3b2d753a9796b47075ed64bda9617681e39d041c
Abduallah Al Jobayer <118735480+jobayer-ee@users.noreply.github.com>
2023-09-19 13:49:02 +0600

    Blynk_Weather_Station.ino
    I updated the Oled status display to monitor real time data.

--- Blynk_Weather_Station.ino
+++ Blynk_Weather_Station.ino
@@ -1,21 +1,30 @@
-#define BLYNK_TEMPLATE_ID "############"
+
+//Updatet the lines according to your blynk account
+
+#define BLYNK_TEMPLATE_ID "########"
 #define BLYNK_TEMPLATE_NAME "#########"
-#define BLYNK_AUTH_TOKEN "###########"
+#define BLYNK_AUTH_TOKEN "###################"
+
+/////////////////////////////////////////
+
 #define BLYNK_PRINT Serial
 #include <WiFi.h>
 #include <DHT.h>
-#define FAN_PIN 18      // Fan pin
+#include <NTPClient.h>
+#include <Wire.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-char auth[] = BLYNK_AUTH_TOKEN;
-
-char ssid[] = "#######";  // type your wifi name
-char pass[] = "######";  // type your wifi password
-
-BlynkTimer timer;
-#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TimeLib.h> // Include the TimeLib library
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+
+
+BlynkTimer timer;
+
+
+#define FAN_PIN 33      // Fan pin
 #define TH 5
 #define LDR 4
 #define BMP_SCK  (13)
@@ -23,16 +32,43 @@ BlynkTimer timer;
 #define BMP_MOSI (11)
 #define BMP_CS   (10)
 
+// Global variable to keep track of LDR state
+bool isLightOn = false;
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+
+// Define your Wi-Fi credentials
+const char* ssid = "#############"; // Update Wifi info
+const char* password = "##########";
+
+
+// Define the NTP server
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 6 * 3600;  // GMT offset for Dhaka (in seconds) - 6 hours ahead of GMT
+const int daylightOffset_sec = 0;  // No daylight offset for Dhaka
+
 DHT dht(TH, DHT22);
 Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
+// Create an NTP client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServer, gmtOffset_sec, daylightOffset_sec);
+
+// Create an OLED display instance
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(100L, sendSensor);
+  // Initialize OLED display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  Blynk.begin(auth, ssid, password);
+   timer.setInterval(100L, sendSensor);
   Serial.begin(9600);
-  while ( !Serial ) delay(100);   // wait for native usb
+while ( !Serial ) delay(100);   // wait for native usb
   Serial.println(F("BMP280 test"));
   dht.begin();
   pinMode(FAN_PIN, OUTPUT);
@@ -56,23 +92,26 @@ void setup() {
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-}
-//Get the DHT11 sensor values
-void DHT11sensor() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  Blynk.virtualWrite(V0, t);
-  Blynk.virtualWrite(V1, h);
+
+  // Initialize NTP client
+  timeClient.begin();
+
+  // Clear the display
+  display.clearDisplay();
+  display.display();
 }
 
 //Get the LDR sensor values
 void LDRsensor() {
   bool value = digitalRead(LDR);
+   isLightOn = (value == 1);
   if (value == 1) {
     WidgetLED LED(V4);
     LED.on();
@@ -100,9 +139,20 @@ void sendSensor()
     Serial.print(bmp.readAltitude(firstFourDigits));/* Adjusted to local forecast! */
     Serial.println(" m");                 //If you don't know it, modify it until you get your current altitude
     Serial.println();
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
     Blynk.virtualWrite(V5, temp);
     Blynk.virtualWrite(V3, pressure);
-    Blynk.virtualWrite(V9, altitude);  //The "1019.66" is the pressure(hPa) at sea level in day in your region
+    Blynk.virtualWrite(V9, altitude);  
+
     if (!isnan(temp) && temp >= 35.0) {
     digitalWrite(FAN_PIN, HIGH);  // Turn on the fan
   } else {
@@ -110,11 +160,78 @@ void sendSensor()
   }
     delay(2000);
 }
-void loop()
-{
-  
-Blynk.run();
-LDRsensor();
-DHT11sensor();
+
+
+void loop() {
+  // Update the NTP client
+  timeClient.update();
+
+  // Get the current time
+  time_t rawTime = timeClient.getEpochTime();
+  struct tm *timeInfo;
+  timeInfo = localtime(&rawTime);
+
+  // Convert to 12-hour format
+  int hour = (hourFormat12(rawTime) == 0) ? 12 : hourFormat12(rawTime);
+  int minute = timeInfo->tm_min;
+  String amPm = (hour < 12) ? "AM" : "PM";
+
+  // Format the time
+  String formattedTime = String(hour) + ":" + (minute < 10 ? "0" : "") + String(minute) + " " + amPm;
+
+  // Get BMP280 temperature
+  float bmpTemperature = bmp.readTemperature();
+
+  // Display the time, BMP280 temperature, DHT22 temperature, humidity, pressure, and altitude
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+ display.setCursor(0, 0);
+  display.print("IP: ");
+  display.println(WiFi.localIP());
+
+  display.setCursor(0, 10);
+  display.print("Time: ");
+  display.print(formattedTime);
+ 
+  // Display BMP280 temperature
+  display.setCursor(0, 20);
+  display.print("T-IN:");
+  int temp =bmp.readTemperature();
+  display.print(temp);
+  display.print(" OUT:");
+  float t = dht.readTemperature();
+  display.print(t);
+  display.print(" C");
+
+  // Display humidity
+  display.setCursor(0, 30);
+  display.print("Humidity: ");
+  float h = dht.readHumidity();
+  display.print(h);
+  display.print("%");
+
+  // Display pressure
+  display.setCursor(0, 40);
+  display.print("Pressure: ");
+    float pressure= bmp.readPressure();
+    float firstFourDigits = (int)(pressure / 100.0) + 1.0;
+  display.print(firstFourDigits);
+  display.print(" Pa");
+
+  // Display altitude
+  display.setCursor(0, 50);
+  display.print("Altitude: ");
+  float altitude=bmp.readAltitude(firstFourDigits);
+  display.print(altitude); // Modify the sea level pressure accordingly
+  display.print(" M");
+
+  display.display();
+  // Update every minute (60 seconds delay)
+  delay(1000);
+
+  Blynk.run();
+  LDRsensor();
   timer.run();
-  }
+}
